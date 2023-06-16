a39dc011e0fa745bb7460ae89fa72d1334ed7c0b
0vercl0k <MichaelHenryRumsfeld@Gmail.com>
2023-06-16 22:16:12 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 0vercl0k on 2023/6/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
